78a2c3694ce8655bd54c860ad93b225e8ad376f8
AnkitRawat9442 <gehurecruitankitrawat@gmail.com>
2022-03-25 16:39:41 +0530

    first program

---
+++ Java Project Ankit/src/unit01/p1_task1_firstjavaprogram.java
@@ -0,0 +1,10 @@
+package unit01;
+
+public class p1_task1_firstjavaprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("hello world ");
+	}
+
+}
