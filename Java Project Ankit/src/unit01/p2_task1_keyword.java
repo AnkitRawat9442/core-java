3401e1a50267f1d1fa1d91ea62646d8ac8d37db4
AnkitRawat9442 <gehurecruitankitrawat@gmail.com>
2022-03-25 16:52:35 +0530

    constant

--- Java Project Ankit/src/unit01/p2_task1_keyword.java
+++ Java Project Ankit/src/unit01/p2_task1_keyword.java
@@ -3,8 +3,29 @@ package unit01;
 public class p2_task1_keyword {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//variable name
+		//Syntax error on token "double", invalid VariableDeclaratorId
+		//identifier can never be any keyword
+		int double=20;
+		
+		//function name
+		//Syntax error on token "new", invalid AnnotationName
+		//identifier can never be any keyword
+		int new() {
+			
+			//sysout[ctrl+space] is the shorcut key for this command
+			System.out.println();
+			return 0;
+		}
+       //keyword can never be used as object name
+	//ABC cannot be resolved to a variable
+     //Syntax error on token "int", delete this token 
+	 ABC int=new ABC;
+	 
+	 //class object can be created inside main even when it's non static
+	 ABC n=new ABC();
+	 n.display();
 	}
 
 }
+
